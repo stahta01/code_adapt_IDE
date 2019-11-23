06532eff3814da98536f3c57f0f63967a6b0242c
Tim Stahlhut <stahta01@gmail.com>
2019-11-23 18:49:00 -0500

    wxSmithContribItems: PCH related code changes

--- src/plugins/contrib/wxSmithContribItems/wxSmithContribItems.h
+++ src/plugins/contrib/wxSmithContribItems/wxSmithContribItems.h
@@ -24,12 +24,6 @@
 #ifndef WXSMITHCONTRIBITEMS_H_INCLUDED
 #define WXSMITHCONTRIBITEMS_H_INCLUDED
 
-// For compilers that support precompilation, includes <wx/wx.h>
-#include <wx/wxprec.h>
-
-#ifndef WX_PRECOMP
-	#include <wx/wx.h>
-#endif
 
 #include <cbplugin.h> // for "class cbPlugin"
 
