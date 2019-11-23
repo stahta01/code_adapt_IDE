6c2e8d3ffb3d1c12860909411312cf152cfac89e
Tim Stahlhut <stahta01@gmail.com>
2019-11-23 18:55:28 -0500

    branding2: Change some names to CodeApapt

--- src/include/branding.h
+++ src/include/branding.h
@@ -5,22 +5,22 @@
 #ifndef BRANDING_H_INCLUDED
 #define BRANDING_H_INCLUDED
 
-#define BRANDING_APP_NAME                  "CodeBlocks"
-#define BRANDING_APP_NAME_FANCY            "Code::Blocks"
-#define BRANDING_APP_LOWERCASE_NAME        "codeblocks"
-#define BRANDING_APP_UPPERCASE_NAME        "CODEBLOCKS"
-#define BRANDING_APP_LOG_FILENAME          "codeblocks.log"
-#define BRANDING_APP_DEBUG_LOG_FILENAME    "codeblocks-debug.log"
-#define BRANDING_APP_CONFIG                "CodeBlocksConfig"
-#define BRANDING_APP_ASSOCIATION           "CodeBlocks."
-#define BRANDING_APP_ENV_DATA_DIR          "CODEBLOCKS_DATA_DIR"
+#define BRANDING_APP_NAME                  "CodeAdapt"
+#define BRANDING_APP_NAME_FANCY            "CodeAdapt"
+#define BRANDING_APP_LOWERCASE_NAME        "codeadapt"
+#define BRANDING_APP_UPPERCASE_NAME        "CODEADAPT"
+#define BRANDING_APP_LOG_FILENAME          "codeadapt.log"
+#define BRANDING_APP_DEBUG_LOG_FILENAME    "codeadapt-debug.log"
+#define BRANDING_APP_CONFIG                "CodeAdaptConfig"
+#define BRANDING_APP_ASSOCIATION           "CodeAdapt."
+#define BRANDING_APP_ENV_DATA_DIR          "CODEADAPT_DATA_DIR"
 #define BRANDING_DDE_SERVICE               wxT_2(BRANDING_APP_UPPERCASE_NAME)
-#define BRANDING_DDE_TOPIC                 wxT_2("CodeBlocksDDEServer")
+#define BRANDING_DDE_TOPIC                 wxT_2("CodeAdaptDDEServer")
 #define BRANDING_EMAIL_CONTACT             "info@codeblocks.org"
-#define BRANDING_STANDARD_DATA_PATH        "/share/codeblocks"
+#define BRANDING_STANDARD_DATA_PATH        "/share/codeadapt"
 #define BRANDING_URL_TICKETS_SITE          "https://sourceforge.net/p/codeblocks/tickets"
 #define BRANDING_URL_WEBSITE               "http://www.codeblocks.org/"
 #define BRANDING_URL_WIKI_SEARCH           "http://wiki.codeblocks.org/index.php"
-#define BRANDING_WIN_STANDARD_DATA_PATH    "\\share\\codeblocks"
+#define BRANDING_WIN_STANDARD_DATA_PATH    "\\share\\codeadapt"
 
 #endif // BRANDING_H_INCLUDED
