6c58f26ac0fa18811ff5deff5ad553dfdff21eb2
Tim Stahlhut <stahta01@gmail.com>
2019-11-23 18:48:53 -0500

    wxContribItems: Add wx_pch.h header

---
+++ src/plugins/contrib/wxContribItems/pch/wx_pch.h
@@ -0,0 +1,39 @@
+/***************************************************************
+ * Name:      wx_pch.h
+ * Purpose:   Header to create Pre-Compiled Header (PCH)
+ **************************************************************/
+
+#ifndef WX_PCH_H_INCLUDED
+#define WX_PCH_H_INCLUDED
+
+#if defined(NOPCH) && defined(USE_PCH)
+    #undef USE_PCH
+#endif // NOPCH
+
+#if ( !defined(USE_PCH) && defined(WX_PRECOMP) )
+    #undef WX_PRECOMP
+#endif // !USE_PCH
+
+#if ( defined(USE_PCH) && !defined(WX_PRECOMP) )
+    #define WX_PRECOMP
+#endif // USE_PCH
+
+// basic wxWidgets headers
+#include <wx/wxprec.h>
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
+#ifdef USE_PCH
+    // put here all your rarely-changing header files
+#endif // USE_PCH
+
+// Fix windows define bugs not fixed in "wx/msw/winundef.h"
+#if defined(__WINDOWS__)
+    #ifdef near
+        #undef near
+    #endif
+#endif
+
+#endif // WX_PCH_H_INCLUDED
