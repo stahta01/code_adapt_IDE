3ca5025936189779ddaa017727c86597f9d778a9
Tim Stahlhut <stahta01@gmail.com>
2019-11-23 18:48:51 -0500

    - sdk: Add branding.h

---
+++ src/include/branding.h
@@ -0,0 +1,26 @@
+/*
+ * This header, branding.h, is too make forking Code::Blocks easier.
+ */
+
+#ifndef BRANDING_H_INCLUDED
+#define BRANDING_H_INCLUDED
+
+#define BRANDING_APP_NAME                  "CodeBlocks"
+#define BRANDING_APP_NAME_FANCY            "Code::Blocks"
+#define BRANDING_APP_LOWERCASE_NAME        "codeblocks"
+#define BRANDING_APP_UPPERCASE_NAME        "CODEBLOCKS"
+#define BRANDING_APP_LOG_FILENAME          "codeblocks.log"
+#define BRANDING_APP_DEBUG_LOG_FILENAME    "codeblocks-debug.log"
+#define BRANDING_APP_CONFIG                "CodeBlocksConfig"
+#define BRANDING_APP_ASSOCIATION           "CodeBlocks."
+#define BRANDING_APP_ENV_DATA_DIR          "CODEBLOCKS_DATA_DIR"
+#define BRANDING_DDE_SERVICE               wxT_2(BRANDING_APP_UPPERCASE_NAME)
+#define BRANDING_DDE_TOPIC                 wxT_2("CodeBlocksDDEServer")
+#define BRANDING_EMAIL_CONTACT             "info@codeblocks.org"
+#define BRANDING_STANDARD_DATA_PATH        "/share/codeblocks"
+#define BRANDING_URL_TICKETS_SITE          "https://sourceforge.net/p/codeblocks/tickets"
+#define BRANDING_URL_WEBSITE               "http://www.codeblocks.org/"
+#define BRANDING_URL_WIKI_SEARCH           "http://wiki.codeblocks.org/index.php"
+#define BRANDING_WIN_STANDARD_DATA_PATH    "\\share\\codeblocks"
+
+#endif // BRANDING_H_INCLUDED
