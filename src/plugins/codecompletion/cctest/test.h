ebb5a6621a297967f02dc0f803d62998b43b58f6
Tim Stahlhut <stahta01@gmail.com>
2020-03-11 21:38:19 -0400

    codecompletion: DELETE folder cctest

--- src/plugins/codecompletion/cctest/test.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-namespace std _GLIBCXX_VISIBILITY(default)
-{
-    _GLIBCXX_BEGIN_NAMESPACE_VERSION
-    void test()
-    {
-    }
-    _GLIBCXX_END_NAMESPACE_VERSION
-} // namespace
